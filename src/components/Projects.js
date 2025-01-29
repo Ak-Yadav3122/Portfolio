39f9a199b73cd5e2d360cdb51675b23b4d4d65d3
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-01-29 15:23:45 +0530

    update image path

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,7 @@ const projects = [
     title: "Music Academy",
     description:
       "The Music Academy project, developed with Next.js, provides an engaging, high-performance platform for music education. Utilizing Next.js’ server-side rendering (SSR) and static site generation (SSG), it ensures fast load times, enhancing user experience. The platform includes personalized user dashboards, enabling students to track progress and enroll in classes",
-    image: "/assets/music-academy-ss.png", // Path to the thumbnail image
+    image: "/assets/music-Academy-ss.png", // Path to the thumbnail image
     video: "/video/musicAcademy.gif", // Path to the video file
 
     liveLink: "https://music-academy-drab.vercel.app/",
@@ -27,7 +27,7 @@ const projects = [
     description:
       "It is a online interview platform and a real-time video call application with secure user authentication, allowing users to sign up, log in, and manage their interviews. With a user-friendly interface, Interviewer can create, edit, and delete enterview schedule, all while enjoying a secure, personalized experience.",
     image: "/assets/video-Interview-ss.png",
-    video: "/video/InterviewPlateform.gif",
+    video: "/video/videoInterview.gif",
     liveLink: "https://interview-plateform.vercel.app/",
     githubLink: "https://github.com/Ak-Yadav3122/Interview-Plateform",
   },
