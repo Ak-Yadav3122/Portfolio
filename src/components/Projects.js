109b504920afbfdec623dd92a49844372ddb4019
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-10 22:18:40 +0530

    update the desc of jobportal project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ const projects = [
     id: 1,
     title: "Music Academy",
     description:
-      "The Music Academy project, developed with Next.js, provides an engaging, high-performance platform for music education. Utilizing Next.js server-side rendering (SSR) and static site generation (SSG), it ensures fast load times, enhancing user experience. The platform includes personalized user dashboards, enabling students to track progress and enroll in classes",
+      "The Music Academy project, developed with Next.js, provides an engaging, high-performance platform for music education. Utilizing Next.js server-side rendering (SSR) and static site generation (SSG), it ensures fast load times, enhancing user experience. The platform includes personalized user dashboards, enabling students to track progress and enroll in classes.",
     image: "/assets/music-Academy-ss.png", // Path to the thumbnail image
     video: "/video/musicAcademy.gif", // Path to the video file
 
@@ -25,7 +25,7 @@ const projects = [
     id: 3,
     title: "Interview-Platform",
     description:
-      "It is a online interview platform and a real-time video call application with secure user authentication, allowing users to sign up, log in, and manage their interviews. With a user-friendly interface, Interviewer can create, edit, and delete interview schedule, all while enjoying a secure, personalized experience.",
+      "Users can register, log in, and manage their interviews using this secure user authentication real-time video call application and online interview platform. Interviewers can establish, modify, and remove interview schedules with ease thanks to an intuitive UI, all while having a safe, customized experience.",
     image: "/assets/video-Interview-ss.png",
     video: "/video/videoInterview.gif",
     liveLink: "https://interview-plateform.vercel.app/",
