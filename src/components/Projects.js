67fc3e1beb3c4122e9201f1c09fd7eb3b3a2a176
Ak-Yadav3122 <aichchhik12@gmail.com>
2025-02-27 14:19:58 +0530

    add the image and video of new project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,7 @@
-
 const projects = [
   {
     id: 1,
-    title: "Music Academy",
+    title: "Aalora-Music Academy",
     description:
       "The Music Academy project, developed with Next.js, provides an engaging, high-performance platform for music education. Utilizing Next.js server-side rendering (SSR) and static site generation (SSG), it ensures fast load times, enhancing user experience. The platform includes personalized user dashboards, enabling students to track progress and enroll in classes.",
     image: "/assets/music-Academy-ss.png", // Path to the thumbnail image
@@ -23,13 +22,24 @@ const projects = [
   },
   {
     id: 3,
-    title: "Interview-Platform",
+    title: "JobSync-Interview Platform",
     description:
       "Users can register, log in, and manage their interviews using this secure user authentication real-time video call application and online interview platform. Interviewers can establish, modify, and remove interview schedules with ease thanks to an intuitive UI, all while having a safe, customized experience.",
     image: "/assets/video-Interview-ss.png",
     video: "/video/videoInterview.gif",
     liveLink: "https://jobsync.aichchhik.xyz",
     githubLink: "https://github.com/Ak-Yadav3122/Interview-Plateform",
+  },
+
+  {
+    id: 3,
+    title: "  Blogify-Blog Platform",
+    description:
+      "Secure user authentication with easy login and logout functionality, Effortlessly manage blog posts by creating, reading, updating, or     deleting content. Engage with other users and manage your content seamlessly andshow appreciation for your favorite posts by liking them.Authors can view details of users who interact with their posts.",
+    image: "/assets/blog-platform.png",
+    video: "/video/blog-platform.gif",
+    liveLink: "https://blogify.aichchhik.xyz",
+    githubLink: "https://github.com/Ak-Yadav3122/blog-application",
   },
 ];
 
